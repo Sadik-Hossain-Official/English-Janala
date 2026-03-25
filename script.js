d5eaaaeab22c8f527efff8e65f8b452d36e86c43
unknown <Sadik-Hossain-Official>
2026-03-25 22:36:20 +0600

    Fixed the responsivenes with animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let hamburger= document.getElementById("hamburger");
 hamburger.addEventListener("click",()=>{
     console.log("Event Clicked!");
-    document.getElementById("menu").classList.toggle("hidden");
+    document.getElementById("menu").classList.toggle("opacity-0");
     document.getElementById("bar1").classList.toggle("rotate-45");
     document.getElementById("bar1").classList.toggle("translate-y-2");
     document.getElementById("bar2").classList.toggle("hidden");
