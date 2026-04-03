98d304f47c409c88795c2997d42c4cbac46c7b45
Sadik-Hossain-Official <sadikhossain230802@gmail.com>
2026-04-03 10:28:33 +0600

    added fetch data in lesson section

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ hamburger.addEventListener("click",()=>{
     document.getElementById("bar3").classList.toggle("-translate-y-1");
 })
 
+// Lesson section functionality
+const loadLessons= ()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all") //gives a promise of responce
+    .then(res=>res.json())// gives a promise to return json data
+    .then(jsonData=>displayLesson(jsonData.data));//finally the data returns and this .then handles the last promise
+}
+function displayLesson(lessons)
+{
+    const lessonContainer=document.getElementById("level-container");
+    lessonContainer.innerHTML="";
+    for(let lesson of lessons)
+    {   
+        const btnDiv=document.createElement("div");
+        btnDiv.innerHTML=`<button href="#" class="border-2 rounded-xl font-semibold px-2 py-1 text-center text-blue-800 items-center hover:bg-sky-600 hover:text-white hover:border-white"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>`;
 
+        lessonContainer.appendChild(btnDiv);
+    }
+}
+loadLessons();
