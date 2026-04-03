e451a0318e20e4f4f4008e2d9c021291e175aa10
Sadik-Hossain-Official <sadikhossain230802@gmail.com>
2026-04-03 10:27:30 +0600

    responsive navbar added

---
+++ script.js
@@ -0,0 +1,11 @@
+// hamburger menu functionalilty responsive
+let hamburger= document.getElementById("hamburger");
+hamburger.addEventListener("click",()=>{
+    console.log("Event Clicked!");
+    document.getElementById("menu").classList.toggle("hidden");
+    document.getElementById("bar1").classList.toggle("rotate-45");
+    document.getElementById("bar1").classList.toggle("translate-y-2");
+    document.getElementById("bar2").classList.toggle("hidden");
+    document.getElementById("bar3").classList.toggle("-rotate-45");
+    document.getElementById("bar3").classList.toggle("-translate-y-1");
+})
